f88baea16b5aa65be2bb421d6a7e234a4ed3ae01
💜 Rihun 💊 <pro382gamer@gmail.com>
2025-10-02 00:14:47 +0600

    Create if-else-condition and indention
    condition and indention

---
+++ if-else-condition.py
@@ -0,0 +1,10 @@
+x11 = 29
+y11 = 29
+
+if  x11 > y11:
+  print('x11 is greater than y11')
+elif x11 == y11:
+  print('x11 and y11 are equal')
+else:
+  print('x11 is not greater than y11')
+#here we need indentation for python.
